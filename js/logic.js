805bd572195f653249856d05baac4743997f9088
Francis <ancis.arcede@gmail.com>
2017-08-28 11:34:46 -0400

    finished up logic to post to html

--- js/logic.js
+++ js/logic.js
@@ -1,4 +1,4 @@
-// Initialize Firebase
+// Initialize and authenticate Firebase
   var config = {
     apiKey: "AIzaSyBFle2boOCQY5McPRwe6IjIiWc3RxrCPsw",
     authDomain: "train-scheduler-833d9.firebaseapp.com",
@@ -12,12 +12,16 @@
 
 	var trainData = firebase.database();
 
-$("#addTrainbtn").on("click", function(){
+// click listener for button to add trains
+$("#addTrainBtn").on("click", function(){
+
+	// grab user input
 	var trainName = $("#trainNameInput").val().trim();
 	var destination = $("#destinationInput").val().trim();
 	var firstTrain = moment($("#firstTrainInput").val().trim(),"HH:mm").subtract(10, "years").format("X");
 	var frequency = $("#frequencyInput").val().trim();
 
+	// stores input for new train data
 	var newTrain = {
 		name: trainName,
 		destination: destination,
@@ -29,6 +33,7 @@ $("#addTrainbtn").on("click", function(){
 
 	alert("Train Added!");
 
+	// Clears all text submission boxes
 	$("#trainNameInput").val("");
 	$("#destinationInput").val("");
 	$("#firstTrainInput").val("");
@@ -36,4 +41,22 @@ $("#addTrainbtn").on("click", function(){
 
 	return false;
 
+})
+
+trainData.ref().on("child_added",function(snapshot){
+	var name = snapshot.val().name;
+	var destination = snapshot.val().destination;
+	var frequency = snapshot.val().frequency;
+	var firstTrain = snapshot.val().firstTrain;
+
+	var remainder = moment().diff(moment.unix(firstTrain), "minutes")%frequency;
+	var minutes = frequency - remainder;
+	var arrival = moment().add(minutes, "m").format("hh:mm A");
+
+	console.log(remainder);
+	console.log(minutes);
+	console.log(arrival);
+
+	$("#trainTable > tBody").append("<tr><td>"+name+"</td><td>"+destination+"</td><td>"+frequency+"</td><td>"+arrival+"</td><td>"+minutes+"</td></tr>");
+	
 })
